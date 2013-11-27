6ef775489f18fea70e5886c13479b6d209af32af
Ian-Woo Kim <ianwookim@gmail.com>
2013-11-27 17:35:18 +0100

    first working nix expression for root5

--- packages/root5/default.nix
+++ packages/root5/default.nix
@@ -1,13 +1,18 @@
-{ stdenv, fetchurl, x11, libX11, file, libXpm }:
+{ stdenv, fetchurl, x11, libX11, file, libXpm, libXft, libXext }:
  
 stdenv.mkDerivation { 
   name = "root5-34.12"; 
   src = fetchurl { 
     url = "ftp://root.cern.ch/root/root_v5.34.12.source.tar.gz";
     sha256 = "b471def39ef2ce032a820e46b4c7a699ca0a7f123f4d975a7bf151b49831e802";
-  }; 
+  };
+  patches = [ ./no-sys-dirs.patch ] ;
+  x11dir = libX11; 
+  XPM = libXpm; 
+  Xftdir = libXft; 
+  Xextdir = libXext;
   #configureFlags = "--with-x11-libdir=${libX11}/lib --with-xpm-libdir=${libXpm}/lib";
-  configureFlags = "--disable-x11 --disable-shared --disable-sqlite --disable-ssl --disable-ruby --disable-afs --disable-alien --disable-afdsmgrd --disable-asimage --disable-astiff --disable-bonjour --disable-roofit --disable-pgsql --disable-pythia6 --disable-pythia8 --disable-opengl --disable-mysql --disable-oracle --disable-python --disable-qt --disable-qtgsi --disable-rfio --disable-shadowpw --disable-tmva --disable-xml --disable-xrootd --disable-ldap --disable-gsl-shared --disable-cocoa --disable-cling --disable-cocoa --disable-gviz --enable-builtin-pcre --enable-builtin-zlib --enable-builtin-lzma --enable-builtin-ftgl --disable-krb5 --disable-genvector --disable-mathmore --disable-memstat --disable-minuit2 --disable-monalisa --disable-odbc --disable-rfio --disable-srp --disable-table --disable-unuran --disable-winrtdebug --disable-xft";
-  buildInputs = [ x11 file libXpm ];
+  configureFlags = "--enable-rpath --disable-shared --disable-sqlite --disable-ssl --disable-ruby --disable-afs --disable-alien --disable-afdsmgrd --disable-asimage --disable-astiff --disable-bonjour --disable-roofit --disable-pgsql --disable-pythia6 --disable-pythia8 --disable-opengl --disable-mysql --disable-oracle --disable-python --disable-qt --disable-qtgsi --disable-rfio --disable-shadowpw --disable-tmva --disable-xml --disable-xrootd --disable-ldap --disable-gsl-shared --disable-cocoa --disable-cling --disable-cocoa --disable-gviz --enable-builtin-pcre --enable-builtin-zlib --enable-builtin-lzma --enable-builtin-ftgl --disable-krb5 --disable-genvector --disable-mathmore --disable-memstat --disable-minuit2 --disable-monalisa --disable-odbc --disable-rfio --disable-srp --disable-table --disable-unuran --disable-winrtdebug --disable-xft";
+  buildInputs = [ x11 file libXpm ]; 
   # doCheck = true;
 }
