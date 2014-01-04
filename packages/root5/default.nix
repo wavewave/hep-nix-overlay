620fa63165042c36927dc0994aaf4d85a7d4b083
Ian-Woo Kim <ianwookim@gmail.com>
2014-01-04 20:50:45 +0100

    remove libpng, libjpeg dep in root5 which makes some impurities with built-in AfterImage package

--- packages/root5/default.nix
+++ packages/root5/default.nix
@@ -1,5 +1,9 @@
-{ stdenv, fetchurl, libX11, file, libXpm, libXft, 
-  libXext, zlib, libjpeg, libpng }:
+{ stdenv, fetchurl, libX11, file, libXpm, libXft
+, libXext
+, zlib
+#, libjpeg
+#, libpng 
+}:
  
 stdenv.mkDerivation { 
   name = "root5-34.12"; 
@@ -16,6 +20,9 @@ stdenv.mkDerivation {
   
   #configureFlags = "--with-x11-libdir=${libX11}/lib --with-xpm-libdir=${libXpm}/lib";
   configureFlags = "--enable-rpath --enable-minuit2 --disable-shared --disable-sqlite --disable-ssl --disable-ruby --disable-afs --disable-alien --disable-afdsmgrd --enable-asimage --disable-astiff --disable-bonjour --disable-roofit --disable-pgsql --disable-pythia6 --disable-pythia8 --disable-opengl --disable-mysql --disable-oracle --disable-python --disable-qt --disable-qtgsi --disable-rfio --disable-shadowpw --disable-tmva --disable-xml --disable-xrootd --disable-ldap --disable-gsl-shared --disable-cocoa --disable-cling --disable-cocoa --disable-gviz --enable-builtin-pcre --disable-builtin-zlib --enable-builtin-lzma --enable-builtin-ftgl --disable-krb5 --disable-genvector --disable-mathmore --disable-memstat  --disable-monalisa --disable-odbc --disable-rfio --disable-srp --disable-table --disable-unuran --disable-winrtdebug --disable-xft";
-  buildInputs = [ libX11 file libXpm zlib libjpeg libpng libXext libXft ]; 
+  buildInputs = [ libX11 file libXpm zlib 
+                  #libjpeg 
+                  #libpng 
+                  libXext libXft ]; 
   # doCheck = true;
 }
