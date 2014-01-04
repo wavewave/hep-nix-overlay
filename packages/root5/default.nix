0ae21d70bdb2590f59e5ee85b11cba76be4d171e
Ian-Woo Kim <ianwookim@gmail.com>
2014-01-04 14:44:26 +0100

    fix impurity in Xext check in root5 installation

--- packages/root5/default.nix
+++ packages/root5/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, x11, libX11, file, libXpm, libXft, 
+{ stdenv, fetchurl, libX11, file, libXpm, libXft, 
   libXext, zlib, libjpeg, libpng }:
  
 stdenv.mkDerivation { 
@@ -12,9 +12,10 @@ stdenv.mkDerivation {
   XPM = libXpm; 
   Xftdir = libXft; 
   Xextdir = libXext;
+  XEXT = libXext;
   
   #configureFlags = "--with-x11-libdir=${libX11}/lib --with-xpm-libdir=${libXpm}/lib";
   configureFlags = "--enable-rpath --enable-minuit2 --disable-shared --disable-sqlite --disable-ssl --disable-ruby --disable-afs --disable-alien --disable-afdsmgrd --enable-asimage --disable-astiff --disable-bonjour --disable-roofit --disable-pgsql --disable-pythia6 --disable-pythia8 --disable-opengl --disable-mysql --disable-oracle --disable-python --disable-qt --disable-qtgsi --disable-rfio --disable-shadowpw --disable-tmva --disable-xml --disable-xrootd --disable-ldap --disable-gsl-shared --disable-cocoa --disable-cling --disable-cocoa --disable-gviz --enable-builtin-pcre --disable-builtin-zlib --enable-builtin-lzma --enable-builtin-ftgl --disable-krb5 --disable-genvector --disable-mathmore --disable-memstat  --disable-monalisa --disable-odbc --disable-rfio --disable-srp --disable-table --disable-unuran --disable-winrtdebug --disable-xft";
-  buildInputs = [ x11 file libXpm zlib libjpeg libpng libXext libXft ]; 
+  buildInputs = [ libX11 file libXpm zlib libjpeg libpng libXext libXft ]; 
   # doCheck = true;
 }
