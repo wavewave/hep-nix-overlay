6d8aa9e19e1ae9a6eeb2e3e76323721d0493d120
Ian-Woo Kim <ianwookim@gmail.com>
2014-01-14 12:13:54 +0100

    root installable on Mac OS X

--- packages/root5/default.nix
+++ packages/root5/default.nix
@@ -1,5 +1,7 @@
 { stdenv, fetchurl, cmake, gfortran, zlib, libX11, libXext, libXpm, libXft
-, libtiff, libjpeg, giflib, libpng, pcre, freetype, python, libxml2
+, libtiff, libjpeg, giflib, libpng, pcre, freetype
+# , python # for the time being
+, libxml2
 , gsl, krb5, openssl, pkgconfig, fftw, sqlite, cfitsio
 }:
  
@@ -10,14 +12,16 @@ stdenv.mkDerivation {
     sha256 = "b471def39ef2ce032a820e46b4c7a699ca0a7f123f4d975a7bf151b49831e802";
   };
   buildInputs = [ cmake gfortran zlib libX11 libXext libXpm libXft pcre freetype 
-                  giflib libtiff libjpeg libpng python gsl krb5 libxml2 openssl
+                  giflib libtiff libjpeg libpng 
+                  # python 
+                  gsl krb5 libxml2 openssl
                   pkgconfig fftw sqlite cfitsio
                 ];
   patches = [ ./darwin.patch ];
   preConfigure = '' 
     substituteInPlace cmake/modules/FindGSL.cmake --replace "/usr/bin/" "" --replace "/usr/bin" "" --replace "/usr/local/bin" "" 
 '';
-  cmakeFlags = "-Dopengl:String=OFF -Dpythia8:String=OFF -Dpythia6:String=OFF -Dpgsql:String=OFF";   
+  cmakeFlags = "-Dopengl:String=OFF -Dpythia8:String=OFF -Dpythia6:String=OFF -Dpgsql:String=OFF -Dpython:String=OFF";   
 
 }
 
