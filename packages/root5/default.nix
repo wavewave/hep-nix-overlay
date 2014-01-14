488c7df9eabcad9d897c7f69887fca1d56ee6dc7
Ian-Woo Kim <ianwookim@gmail.com>
2014-01-14 09:06:05 +0100

    pseudo working cmake root install (impurity exists!)

--- packages/root5/default.nix
+++ packages/root5/default.nix
@@ -1,5 +1,7 @@
 { stdenv, fetchurl, cmake, gfortran, zlib, libX11, libXext, libXpm, libXft
-, libtiff, libjpeg, libpng, pcre, freetype, python }:
+, libtiff, libjpeg, giflib, libpng, pcre, freetype, python, libxml2
+, gsl, krb5, openssl, pkgconfig, fftw, sqlite, cfitsio
+}:
  
 stdenv.mkDerivation { 
   name = "root5-34.12"; 
@@ -8,36 +10,15 @@ stdenv.mkDerivation {
     sha256 = "b471def39ef2ce032a820e46b4c7a699ca0a7f123f4d975a7bf151b49831e802";
   };
   buildInputs = [ cmake gfortran zlib libX11 libXext libXpm libXft pcre freetype 
-                  libtiff libjpeg libpng python 
+                  giflib libtiff libjpeg libpng python gsl krb5 libxml2 openssl
+                  pkgconfig fftw sqlite cfitsio
                 ];
+  patches = [ ./darwin.patch ];
+  preConfigure = '' 
+    substituteInPlace cmake/modules/FindGSL.cmake --replace "/usr/bin/" "" --replace "/usr/bin" "" --replace "/usr/local/bin" "" 
+'';
+  # cmakeFlags = "-DX11_FIND_QUIETLY=OFF";   
+
 }
 
 
-# system
-# , libX11, file, libXpm, libXft, libXext, zlib, gfortran
-#, libjpeg
-#, libpng 
-#}:
-
-
-#  patches = [ ./no-sys-dirs.patch ] ;
-#  x11dir = libX11; 
-#  XPM = libXpm; 
-#  Xftdir = libXft; 
-#  Xextdir = libXext;
-#  XEXT = libXext;
-
-#  patchDarwin64 = "arch=macosx64";
-#  preConfigure = if system != "x86_64-darwin" then ""
-#                 else ''
-#  substituteInPlace configure --subst-var patchDarwin64
-# '';
-
-  
-#  #configureFlags = "--with-x11-libdir=${libX11}/lib --with-xpm-libdir=${libXpm}/lib";
-#  configureFlags = "--enable-rpath --enable-minuit2 --disable-fink --disable-shared --disable-sqlite --disable-ssl --disable-ruby --disable-afs --disable-alien --disable-afdsmgrd --enable-asimage --disable-astiff --disable-bonjour --disable-roofit --disable-pgsql --disable-pythia6 --disable-pythia8 --disable-opengl --disable-mysql --disable-oracle --disable-python --disable-qt --disable-qtgsi --disable-rfio --disable-shadowpw --disable-tmva --disable-xml --disable-xrootd --disable-ldap --disable-gsl-shared --disable-cocoa --disable-cling --disable-cocoa --disable-gviz --enable-builtin-pcre --disable-builtin-zlib --enable-builtin-lzma --enable-builtin-ftgl --disable-krb5 --disable-genvector --disable-mathmore --disable-memstat  --disable-monalisa --disable-odbc --disable-rfio --disable-srp --disable-table --disable-unuran --disable-winrtdebug --disable-xft";
-#  buildInputs = [ libX11 file libXpm zlib 
-#                  #libjpeg 
-#                  #libpng 
-#                  libXext libXft gfortran ]; 
-  # doCheck = true;
