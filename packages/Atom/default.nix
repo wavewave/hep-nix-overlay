d405030b438f2354cee37bb87baced951b23bd12
Ian-Woo Kim <ianwookim@gmail.com>
2014-01-15 20:43:11 +0100

    unnecessary libdl dependency removed for Mac OS X

--- packages/Atom/default.nix
+++ packages/Atom/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchgit, fetchfile, cmake, root5, HepMC, gsl, FastJet, pkgconfig, 
-  python, cython0192, libyamlcppPIC,  boost
+  python, cython0192, libyamlcppPIC,  boost, 
 #, doxygen 
 }:
  
@@ -18,14 +18,14 @@ stdenv.mkDerivation {
     rev = "11f0efee302ba46ee9832c1bad2b947dac9f9712";
     sha256 = "50e8d375777a916b63ecd38a7790534099d2a8befe9ee2d2d61fcc668651d443" ;
   };
-  patches = [ ./findYamlCpp.patch ]; # ./findHepMC.patch 
+  patches = [ ./findYamlCpp.patch ./findROOT.patch ]; # ./findHepMC.patch 
   buildInputs = [ cmake root5 HepMC gsl FastJet pkgconfig libyamlcppPIC 
-                  python cython0192 boost # Rivet
+                  python cython0192 boost 
                   # doxygen
                 ];
   pkgconfigDepends = [ libyamlcppPIC ] ;
   # patches = [ ];
-  cmakeFlags = ''-DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-undefined,dynamic_lookup"  -DCMAKE_VERBOSE_MAKEFILE=ON -DYamlCpp_STATIC_LIBRARY=TRUE -DYamlCpp_DIR=${libyamlcppPIC} -DBoost_DIR=${boost} -DHEPMC_DIR=${HepMC} -DHEPMC_ROOT_DIR=${HepMC}'';
+  cmakeFlags = ''-DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-undefined,dynamic_lookup"  -DCMAKE_VERBOSE_MAKEFILE=ON -DYamlCpp_STATIC_LIBRARY=TRUE -DYamlCpp_DIR=${libyamlcppPIC} -DBoost_DIR=${boost} -DHEPMC_DIR=${HepMC} -DHEPMC_ROOT_DIR=${HepMC} '';
 
   meta = { 
     priority  = "9";
