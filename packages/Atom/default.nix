1cd09aff267638c6d5b9fe7845566214f5486639
Ian-Woo Kim <ianwookim@gmail.com>
2014-01-16 20:34:00 +0100

    no doxygen for Atom (for purity)

--- packages/Atom/default.nix
+++ packages/Atom/default.nix
@@ -19,7 +19,7 @@ stdenv.mkDerivation {
     rev = "11f0efee302ba46ee9832c1bad2b947dac9f9712";
     sha256 = "50e8d375777a916b63ecd38a7790534099d2a8befe9ee2d2d61fcc668651d443" ;
   };
-  patches = [ ./findYamlCpp.patch ./findROOT.patch ]; # ./findHepMC.patch 
+  patches = [ ./findYamlCpp.patch ./findROOT.patch ./noDoxygen.patch ]; # ./findHepMC.patch 
   buildInputs = [ cmake root5 HepMC gsl FastJet pkgconfig libyamlcppPIC 
                   python cython0192 boost 
                 # graphviz
