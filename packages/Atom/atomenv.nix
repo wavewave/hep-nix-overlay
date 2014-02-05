e0bae9da67a2395d5b18afaf8f2227d34b50d860
Ian-Woo Kim <ianwookim@gmail.com>
2014-02-05 22:48:08 +0100

    add LD_LIBRARY_PATH= for linux system for env-atom

--- packages/Atom/atomenv.nix
+++ packages/Atom/atomenv.nix
@@ -8,8 +8,13 @@ pkgs.myEnvFun {
     boost
     stdenv
   ];
-  extraCmds = with pkgs; ''
+  
+  extraCmds = with pkgs; if(stdenv.isDarwin) then ''
     source ${Atom}/bin/atomenv.sh
     export DYLD_LIBRARY_PATH=${stdenv.gcc.gcc}/lib:${Atom}/lib:${boost}/lib:$DYLD_LIBRARY_PATH
+  ''
+    else ''
+    source ${Atom}/bin/atomenv.sh
+    export LD_LIBRARY_PATH=
   '';
 }
