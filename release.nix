a3760c43c45337b3100df715907b80c8d9da77fd
Ian-Woo Kim <ianwookim@gmail.com>
2014-02-16 09:11:51 -0800

    add release.nix

---
+++ release.nix
@@ -0,0 +1,7 @@
+{ nixpkgs }: # , hepNixOverlay }: 
+
+let pkgs = import nixpkgs { system = "x86_64-linux"; };
+    #heppkgs = import hepNixOverlay ; 
+    systems = [ "x86_64-linux" ]; 
+    jobs = { hello = pkgs.hello }; 
+in jobs 
