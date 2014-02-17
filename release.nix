7446c7fc336015e9116ea33dda494e04f3275e33
Ian-Woo Kim <ianwookim@gmail.com>
2014-02-17 01:34:35 +0100

    correct argument name 'pkgs' to call hep-nix-overlay

--- release.nix
+++ release.nix
@@ -3,7 +3,7 @@
 let pkgs_linux64 = import nixpkgs { system = "x86_64-linux"; };
     pkgs_darwin64 = import nixpkgs { system = "x86_64-darwin"; };
 
-    heppkgs_linux64 = import hepNixOverlay { inherit pkgs_linux64; };
-    heppkgs_darwin64 = import hepNixOverlay { inherit pkgs_darwin64; }; 
+    heppkgs_linux64 = import hepNixOverlay { pkgs = pkgs_linux64; };
+    heppkgs_darwin64 = import hepNixOverlay { pkgs = pkgs_darwin64; }; 
     jobs = heppkgs_darwin64; 
 in jobs
