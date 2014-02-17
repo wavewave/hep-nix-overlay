b86a94ffb95b723421485c6aca9092cbdb59fa6e
Ian-Woo Kim <ianwookim@gmail.com>
2014-02-17 01:26:54 +0100

    release.nix changed

--- release.nix
+++ release.nix
@@ -5,6 +5,5 @@ let pkgs_linux64 = import nixpkgs { system = "x86_64-linux"; };
 
     heppkgs_linux64 = import hepNixOverlay { inherit pkgs_linux64; };
     heppkgs_darwin64 = import hepNixOverlay { inherit pkgs_darwin64; }; 
-    #systems = [ "x86_64-linux" ]; 
-    jobs = heppkgs_darwin64; #{ HepMC = heppkgs.HepMC; }; 
+    jobs = heppkgs_darwin64; 
 in jobs
