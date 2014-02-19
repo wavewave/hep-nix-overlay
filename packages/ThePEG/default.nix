5181dacdc2c7063d5b475dbc357bcda1f98c643b
Ian-Woo Kim <ianwookim@gmail.com>
2014-02-19 02:08:25 +0100

    add libyamlcppPIC in the dependency of ThePEG

--- packages/ThePEG/default.nix
+++ packages/ThePEG/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, fetchurl, gsl, zlib, HepMC, LHAPDF, Rivet, FastJet }:
+{ stdenv, fetchurl, gsl, zlib, HepMC, LHAPDF, Rivet, FastJet
+, libyamlcppPIC }:
  
 stdenv.mkDerivation { 
   name = "ThePEG"; 
@@ -7,6 +8,6 @@ stdenv.mkDerivation {
     url = "http://www.hepforge.org/archive/thepeg/ThePEG-1.9.0.tar.bz2";
     sha256 = "0p8af2jdbn6pil0jzrr2yf38w8nzf35116hvvxkla61679g8xr9y"; 
   };
-  buildInputs = [ gsl zlib HepMC LHAPDF Rivet FastJet ];
+  buildInputs = [ gsl zlib HepMC LHAPDF Rivet FastJet libyamlcppPIC ];
   configureFlags = "--with-zlib=${zlib} --with-lhapdf=${LHAPDF} --with-hepmc=${HepMC} --with-rivet=${Rivet} --with-fastjet=${FastJet} " ;  
 }
