b009cd3fe7031ca925a44897597490142b31b8f0
Ian-Woo Kim <ianwookim@gmail.com>
2014-04-15 18:11:03 +0200

    YODA: 1.0.4 -> 1.0.6

--- packages/YODA/default.nix
+++ packages/YODA/default.nix
@@ -2,11 +2,12 @@
  
 stdenv.mkDerivation rec { 
   name = "YODA-${version}"; 
-  version = "1.0.4";
+  version = "1.0.6";
 
   src = fetchurl { 
-    url = "http://www.hepforge.org/archive/yoda/YODA-1.0.4.tar.gz";
-    md5 = "0b3315b7ebfcfdcd0f2b973bca426d68";
+    url = "http://www.hepforge.org/archive/yoda/YODA-1.0.6.tar.gz";
+    #md5 = "0b3315b7ebfcfdcd0f2b973bca426d68";
+    sha256 = "0543zgnqv75ff5npz25djwrjpc0vzkghhgs0x21kmhwlk8mpc1m2";
   };
   #patches = [ ]; 
 
