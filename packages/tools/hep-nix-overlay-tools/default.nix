2481ab21b35a64f7be0150882b8a68a9654ddc63
Ian-Woo Kim <ianwookim@gmail.com>
2014-05-08 18:22:26 +0200

    hep-nix-overlay-tools: bump up commit

--- packages/tools/hep-nix-overlay-tools/default.nix
+++ packages/tools/hep-nix-overlay-tools/default.nix
@@ -6,8 +6,8 @@ hs.cabal.mkDerivation (self: {
   pname = "hep-nix-overlay-tools";
   version = "0.0";
   src = fetchgit { url = "https://github.com/wavewave/hep-nix-overlay-tools.git"; 
-                   rev = "4ee0e233a9131f1d5ba6df1310f9820dca908b33";
-                   sha256 = "af1c44d15d719fd499c9baacc643540ec085bfeb399ed2d43fc005e0a74f67b1";
+                   rev = "2b85878eb31cdeda9384ca5b6e378303055d6afa";
+                   sha256 = "d7d167f68821c0294f8a28f1c9aa5e7bf34338a7814d50c503a7f333d24cd1a3";
                  };  
   isLibrary = false;
   isExecutable = true;
