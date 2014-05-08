a3f71af8db73bc0f67e2eac78f55f58d19ea2f11
Ian-Woo Kim <ianwookim@gmail.com>
2014-05-08 17:54:38 +0200

    hep-nix-overlay-tools: updated with correct behavior for NIX_PATH

--- packages/tools/hep-nix-overlay-tools/default.nix
+++ packages/tools/hep-nix-overlay-tools/default.nix
@@ -6,12 +6,13 @@ hs.cabal.mkDerivation (self: {
   pname = "hep-nix-overlay-tools";
   version = "0.0";
   src = fetchgit { url = "https://github.com/wavewave/hep-nix-overlay-tools.git"; 
-                   rev = "f169317668a19463f55e942a4aa3cde50615acd1";
-                   sha256 = "edd04d3b942926d2d443fcd3426985ccc56b8d7d67c0eb139ee0254786d9dd1f";
+                   rev = "4ee0e233a9131f1d5ba6df1310f9820dca908b33";
+                   sha256 = "af1c44d15d719fd499c9baacc643540ec085bfeb399ed2d43fc005e0a74f67b1";
                  };  
   isLibrary = false;
   isExecutable = true;
   buildDepends = [ hs.cmdargs
+                   hs.split
                  ];
   doCheck = false;
   meta = {
