335728eb088dba057d7f4fe96759b6f9220bd6d0
Ian-Woo Kim <ianwookim@gmail.com>
2014-05-13 20:46:49 +0200

    LHAPDF: no python and swig on OS X

--- packages/LHAPDF/default.nix
+++ packages/LHAPDF/default.nix
@@ -7,8 +7,10 @@ stdenv.mkDerivation rec {
     url = "http://www.hepforge.org/archive/lhapdf/lhapdf-5.9.1.tar.gz";
     sha256 = "174ihr8cz02h4acdw65f9cprfd2m3kplfs5b5b72fmpjsx3b1fc6";
   };
+  enableParallelBuilding = true;
   patches = [./use_cxx.patch];
-  buildInputs = [ gfortran python swig ];
+ 
+  buildInputs = if (stdenv.isDarwin) then [ gfortran ] else [ gfortran python swig ];
    
   # configureFlags = "--without-HepMC";
 }
