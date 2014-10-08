2990d7249fdf83462f8b2c5c8fc9c2df33eb6476
Ian-Woo Kim <ianwookim@gmail.com>
2014-10-08 18:34:59 +0200

    revert allowUnfree

--- release.nix
+++ release.nix
@@ -1,7 +1,7 @@
 { nixpkgs_linux64, nixpkgs_darwin64, hepNixOverlay }: 
 
 let pkgs_linux64 = import nixpkgs_linux64 { system = "x86_64-linux"; };
-    pkgs_darwin64 = import nixpkgs_darwin64 { system = "x86_64-darwin"; allowUnfree = true; };
+    pkgs_darwin64 = import nixpkgs_darwin64 { system = "x86_64-darwin"; };
 
     heppkgs_linux64 = import hepNixOverlay { pkgs = pkgs_linux64; };
     heppkgs_darwin64 = import hepNixOverlay { pkgs = pkgs_darwin64; }; 
