25b6e232dc22e7d6870c789235bc6f4c6fcc9336
cbpark <cbpark@gmail.com>
2015-02-07 23:14:02 +0900

    Update and fix CheckMATE

--- pkgs/CheckMATE/default.nix
+++ pkgs/CheckMATE/default.nix
@@ -1,26 +1,21 @@
 { pkgs, fetchurl, root5 }:
- 
 
-pkgs.stdenv.mkDerivation rec { 
-  name = "CheckMATE-${version}"; 
-  version = "1.1.10";
-  src = fetchurl { 
-    url = "http://www.hepforge.org/archive/checkmate/CheckMATE-1.1.10.tar.gz";
-    sha256 = "0yw0y03bdvw33xx3nkkdy2fn3y6sv0lazbq8swy1c3cpd0y0x3lx";
+pkgs.stdenv.mkDerivation rec {
+  name = "CheckMATE-${version}";
+  version = "1.1.16";
+  src = fetchurl {
+    url = "http://www.hepforge.org/archive/checkmate/CheckMATE-1.1.16.tar.gz";
+    sha256 = "1hd205h7k5ri2svbm3n2v8xi1fzjg294ilpkj4i6s517nrf9fa8n";
   };
-  buildInputs = [ root5 pythonCheckMATE pkgs.which pkgs.tcl ];
-  enableParallelBuilding = true; 
+  buildInputs = [ root5 ] ++ (with pkgs; [ which tcl python pythonPackages.numpy]);
+  enableParallelBuilding = true;
 
-  pythonCheckMATE = pkgs.pythonFull.override { 
-                      extraLibs = with pkgs.pythonPackages; [ numpy ];
-                    };
-
-  installPhase = ''        
+  installPhase = ''
     cd ..
     tar cvzf CheckMATE-${version}.tar.gz $sourceRoot
     mkdir -p $out/share/CheckMATE-${version}
     cp CheckMATE-${version}.tar.gz $out/share/CheckMATE-${version}
     mkdir -p $out/lib
     cp $sourceRoot/tools/delphes/Delphes-3.0.10X/libDelphes.so $out/lib
-  ''; 
+  '';
 }
