6282b5231f7fcc4e19b1da32dc98440497ddd65a
cbpark <cbpark@gmail.com>
2015-02-10 01:09:56 +0900

    Use cmake for HepMC

--- pkgs/HepMC/default.nix
+++ pkgs/HepMC/default.nix
@@ -1,16 +1,18 @@
-{ stdenv, fetchurl, m4, automake, autoconf, libtool }: 
+{ stdenv, fetchurl, cmake }:
 
-stdenv.mkDerivation rec { 
-  name = "HepMC-${version}"; 
+stdenv.mkDerivation rec {
+  name = "HepMC-${version}";
   version = "2.06.08";
-  src = fetchurl { 
+  src = fetchurl {
     url = "http://lcgapp.cern.ch/project/simu/HepMC/download/HepMC-2.06.08.tar.gz";
     sha256 = "1km8m0xkx6igz2rp5yvm5sjnczny8r5vd2xqvl3my10a7rww3rlb";
   };
-  nativeBuildInputs = [ m4 autoconf automake libtool ]; 
-  patches = [ ./no-doc-gen.patch ];
-  configureFlags = "--with-momentum=GEV --with-length=MM --enable-static";
-  preConfigure = ''
-sh ${./preconfigure.sh} 
-'';
+  patches = [ ./in_source_error.patch ];
+  buildInputs = [ cmake ];
+  enableParallelBuilding = true;
+
+  cmakeFlags = [
+    "-Dmomentum:STRING=GEV"
+    "-Dlength:STRING=MM"
+  ];
 }
