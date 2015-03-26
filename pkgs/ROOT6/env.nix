ebc4907ec0f14cfadee403253934b8d915381fc6
Ian-Woo Kim <ianwookim@gmail.com>
2015-03-26 16:55:00 +0100

    ROOT6: works on linux

--- pkgs/ROOT6/env.nix
+++ pkgs/ROOT6/env.nix
@@ -6,10 +6,14 @@ in pkgs.myEnvFun rec {
 
   buildInputs = with pkgs; [ ROOT6 pythonFull binutils stdenv.cc ] ++ (if stdenv.isDarwin then [darwin.sw_vers] else []);
   
-  extraCmds = ''
+  extraCmds = if pkgs.stdenv.isDarwin then ''
     export NIX_CFLAGS_COMPILE="-target x86_64-apple-darwin11.4.2 $NIX_CFLAGS_COMPILE"
     export NIX_CFLAGS_LINK="-target x86_64-apple-darwin11.4.2 $NIX_CFLAGS_LINK"
-
     source ${ROOT6}/bin/thisroot.sh
+  '' else ''
+    source ${ROOT6}/bin/thisroot.sh
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc}/lib:$LD_LIBRARY_PATH
   '';
 }
+
+#    export INCLUDE_PATH=${pkgs.stdenv.cc.cc}/include:$INCLUDE_PATH
