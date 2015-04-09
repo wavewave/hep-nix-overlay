b27ea13538ae33139921398f1f1c70784b9aac74
cbpark <cbpark@gmail.com>
2015-04-09 17:02:52 +0900

    Delphes: 3.1.2 -> 3.2.0

--- pkgs/Delphes/default.nix
+++ pkgs/Delphes/default.nix
@@ -1,14 +1,18 @@
-{ pkgs, fetchurl, root5 }:
- 
-pkgs.stdenv.mkDerivation rec { 
-  name = "Delphes-${version}"; 
-  version = "3.1.2";
-  src = fetchurl { 
-    url = "http://cp3.irmp.ucl.ac.be/downloads/Delphes-3.1.2.tar.gz";
-    sha256 = "07ga2yvn0h6x1c66yj9v88b0gcfgg5n2rf3dx03kcvb6yx3wrz7d";
+{ pkgs, root5 }:
+
+with pkgs;
+
+pkgs.stdenv.mkDerivation rec {
+  name = "Delphes-${version}";
+  version = "3.2.0";
+  src = fetchurl {
+    url = "http://cp3.irmp.ucl.ac.be/downloads/Delphes-3.2.0.tar.gz";
+    sha256 = "1r6d1jl7mggzm5sbj4n5545vjj2xlanlq0cvbx1200bm5j2v041m";
   };
-  phases = [ "unpackPhase" "buildPhase" "installPhase" ]; 
-  buildInputs = [ root5 pkgs.which ];
+  phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+  buildInputs = [ root5 which ];
+  enableParallelBuilding = true;
+
   buildPhase = ''
     export ROOTSYS=${root5}
     make
@@ -16,6 +20,6 @@ pkgs.stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/bin
     cp  DelphesHepMC DelphesLHEF DelphesSTDHEP Example1 hepmc2pileup lhco2root libDelphes.so pileup2root root2lhco root2pileup stdhep2pileup $out/bin
-    cp -a doc examples python modules readers display classes $out/
-  ''; 
+    cp -a cards doc examples python modules readers display classes $out/
+  '';
 }
