8ebd5d008fede4d78e1e1e136dbaac8cf09bba5e
cbpark <cbpark@gmail.com>
2015-04-10 15:47:01 +0900

    SPheno: 3.3.5 -> 3.3.6

--- pkgs/SPheno/default.nix
+++ pkgs/SPheno/default.nix
@@ -1,21 +1,26 @@
-{ pkgs, stdenv, fetchurl }:
+{ pkgs }:
 
 with pkgs;
 
 stdenv.mkDerivation rec {
   name = "SPheno-${version}";
-  version = "3.3.5";
+  version = "3.3.6";
   src = fetchurl {
-    url = "http://www.hepforge.org/archive/spheno/SPheno-3.3.5.tar.gz";
-    sha256 = "0nisc67qlhc5cfrfbjill1bmsyn8maxdlkj3varxqfwzxn7bwcg0";
+    url = "http://www.hepforge.org/archive/spheno/SPheno-3.3.6.tar.gz";
+    sha256 = "17vs51h0ndzlr2j8kksaali5ggslq9n5bp61bwa5sd4mrssn3vj7";
   };
-  patches = [ ./use-gfortran.patch ];
+  patches = [ ];
   buildInputs = [ gfortran ];
   enableParallelBuilding = false;
 
+  configurePhase = ''
+    substituteInPlace Makefile --replace "# F90 = gfortran" "F90 = gfortran"
+    substituteInPlace Makefile --replace "F90 = ifort" "# F90 = ifort"
+  '';
+
   installPhase = ''
     mkdir -p $out
-    cp -r bin doc lib include input output $out
+    cp -a bin doc lib include input output $out
   '';
 
   dontFixup = true;
